4c218e0da528af5a22ebb636a9de53eac83b500f
Anzor <slevin-82@mail.ru>
2025-06-04 10:52:35 +0300

    add

--- src/core/Stock.js
+++ src/core/Stock.js
@@ -13,6 +13,7 @@ export class Stock {
     element.className = "stock";
     element.id = "stock";
     // span.innerHTML = '<svg viewBox="0 0 24 24" width="90"><path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"/></svg>';
+    span.textContent = '↺';
     span.classList.add('stock-span');
     element.appendChild(span);
     // element.style.left = "20px";
